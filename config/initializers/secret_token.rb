e046926226832c403e6f6d13efbe0f3b32e453b7
Derek Hopper <hopper.derek@gmail.com>
2011-03-26 10:19:28 -0400

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beer4textbooks::Application.config.secret_token = '57476b3344d69e89f32479e72fca9adda15d51112ff74ca3de78f0878d24c6f475bcf1826c85bf60df39fc4d768d741d67b971194cc9138ad60688c226735875'

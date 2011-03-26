efd23cca4b97cd337e6838e303a35601b7afca85
Derek Hopper <hopper.derek@gmail.com>
2011-03-26 18:58:38 -0400

    Changing to xml.

--- app/models/books.rb
+++ app/models/books.rb
@@ -4,13 +4,13 @@ class Books < ActiveRecord::Base
     book = HTTParty.get("http://api.campusbooks.com/11/rest/bookinfo",
       :query => {:isbn => isbn,
         :image_height => '100', :image_width => '80',
-        :format => 'json', :key => API_KEY})["response"]
+        :format => 'xml', :key => API_KEY})["response"]
   end
   
   def self.getPricesByIsbn(isbn)
     prices = HTTParty.get("http://api.campusbooks.com/11/rest/prices",
       :query => {:isbn => isbn,
-        :format => 'json', :key => API_KEY})["response"]
+        :format => 'xml', :key => API_KEY})["response"]
   end
   
   def self.getSearchPages(keywords)
@@ -19,7 +19,7 @@ class Books < ActiveRecord::Base
         :query => {
           :keywords => keywords,
           :image_height => '100', :image_width => '80',
-          :format => 'json', :key => API_KEY})["response"]
+          :format => 'xml', :key => API_KEY})["response"]
     rescue Crack::ParseError => e
       nil
     end
@@ -30,14 +30,14 @@ class Books < ActiveRecord::Base
       :query => {
         :keywords => keywords, 
         :page => page, :image_height => '100', :image_width => '80',
-        :format => 'json', :key => API_KEY})["response"]
+        :format => 'xml', :key => API_KEY})["response"]
   end
 
   def self.getBuybackInfoByIsbn(isbn)
     price = HTTParty.get("http://api.campusbooks.com/11/rest/buybackprices",
       :query => {:isbn => isbn,
         :image_width => 200,
-        :format => 'json', :key => API_KEY})["response"]
+        :format => 'xml', :key => API_KEY})["response"]
   end
   
   def self.isIsbn?(input)
